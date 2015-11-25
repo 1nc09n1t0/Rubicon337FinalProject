14e5c5e4427594ec2c70109479f5f298592be32b
Kristoffer Cabulong <kcabulong@email.arizona.edu>
2015-11-24 20:05:38 -0700

    Finished methods for handling the review table

--- DatabaseAdaptor.php
+++ DatabaseAdaptor.php
@@ -63,7 +63,7 @@
 
 			1) getAllReviewsAsArray(): Returns EVERY review in the db:
 			2) getAllReviewsAsArrayByMovieTitle(movie_title): Returns all reviews for a given movie's 		title
-			3) InsertReview(reviewerName,review):
+			3) 3) createReview(movie_title,review, author): Insert a new movie review
 			4) deleteReview(id): 
 			5) updateReview(id, review):
 
@@ -86,101 +86,40 @@
 			return $stmt->fetchAll ( PDO::FETCH_ASSOC );
 		}
 
-		/*
-			MOVIES TABLE FUNCTIONS
-		*/
-
-		// Insert a new quote into the database
-		public function addNewQuote($quote, $author) {
-			$stmt = $this->DB->prepare ( "INSERT INTO quote (added, quote, author, rating, flagged ) values(now(), :quote, :author, 0, 'f')" );
-			$stmt->bindParam ( 'quote', $quote );
+		// 3) createReview(movie_title,review, author): Insert a new movie review
+		public function createReview($movie_title, $review, $author) {
+			$stmt = $this->DB->prepare ( "INSERT INTO reviews (movie_title, review, author) values (:movie_title, :review, :author)" );
+			$stmt->bindParam ( 'movie_title', $movie_title );
+			$stmt->bindParam ( 'review', $review );
 			$stmt->bindParam ( 'author', $author );
 			$stmt->execute ();
 		}
-		
-		// Raise the rating of the quote with the given $ID by 1
-		public function raiseRating($ID) {
-			$sql = "UPDATE quote SET rating=rating+1 WHERE id= :ID";
-			$stmt->bindParam ( 'ID', $ID );
-			$this->DB->exec ( $sql );
-		}
-		
-		// Lower the rating of the quote with the given $ID by 1
-		public function lowerRating($ID) {
-			$stmt = $this->DB->prepare ( "UPDATE quote SET rating=rating-1 WHERE id= :ID" );
-			$stmt->bindParam ( 'ID', $ID );
-			$stmt->execute ();
-		}
-		
-		// Set the 'flagged' column of one particular quote to 't' so it will not
-		// be shown in the quote collection, until all quotes are unflagged later.
-		public function flag($ID) {
-			$stmt = $this->DB->prepare ( "UPDATE quote SET flagged = 't' WHERE id= :ID" );
-			$stmt->bindParam ( 'ID', $ID );
-			$stmt->execute ();
-		}
-		
-		// Used for testing only so far on 9-Nov-2015
-		public function isFlagged($ID) {
-			$stmt = $this->DB->prepare ( "SELECT * FROM quote WHERE id= :ID" );
-			$stmt->bindParam ( 'ID', $ID );
-			$stmt->execute ();
-			$currentRecord = $stmt->fetch ();
-			return $currentRecord ['flagged'] === 't';
-		}
-		
-		// Used for testing only so far on 9-Nov-2015
-		public function removeAllDuckTypedRecords() {
-			$stmt = $this->DB->prepare ( "DELETE FROM users WHERE username LIKE '%duckTyped%'" );
-			$stmt->execute ();
-		}
-		
-		// ////////////////// Implement the next three functions /////////////////////////////
-		
-		// Change all quote records such that the flagged column is 'f' for every one
-		public function unflagAll() {
-			$stmt = $this->DB->prepare ( "UPDATE quote SET flagged = 'f' WHERE id=id" );
-			$stmt->execute ();
-		}
-		
-		// Add a new user with the given $username and the plain text $password, that will be used in the
-		// PHP function password_hash. Before you register new users, you will need this table added to your
-		// data base. Run this in mysql (MariaDB) from the command line after the sql command USE quotes:
-		//
-		// CREATE TABLE users (
-		// id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-		// username varchar(64) NOT NULL default '',
-		// hash varchar(255) NOT NULL default ''
-		// );
-		//
-		// precondition: $username is unique. Change this for the final project.
-		//
-		// Hint: You will need the PHP function
-		//    $hash = password_hash($password, PASSWORD_DEFAULT)
-		// where you put $hash into a column of the data base of users.
-		public function register($username, $password) {
-			$hashed_pwd = password_hash($password, PASSWORD_DEFAULT);
-			$stmt = $this->DB->prepare ( "INSERT INTO users (username, hash) values (:username, :hashed_pwd)" );
-			$stmt->bindParam ( 'username', $username );
-			$stmt->bindParam ( 'hashed_pwd', $hashed_pwd );
-			$stmt->execute ();
-		}
-		
-		// Return TRUE if the given $username has a plain text $password that works with
-		// the hash value stored for that user. 
-		// Hint: You will need the PHP function 
-		//   $success = password_verify($password, $hash)
-		// where $hash is retreived from the data base of users.
-		public function verified($username, $password) {
 
-			$stmt = $this->DB->prepare ( "SELECT * FROM users WHERE username= :username" );
-			$stmt->bindParam ( 'username', $username );
+		// 4) deleteReview(id): deletes a review given by an id. id should be a hidden value on an element in the html. Hopefully.
+		public function deleteReview($id) {
+			$stmt = $this->DB->prepare ( "DELETE FROM reviews WHERE id=:id" );
+			$stmt->bindParam ( 'id', $id );
 			$stmt->execute ();
-			$currentRecord = $stmt->fetch ();
-			$hash = $currentRecord ['hash'];
-
-			return password_verify($password, $hash);
 		}
+
+		// 5) updateReview(id, review):
+		public function updateReview($id, $review){
+			$stmt = $this->DB->prepare ( "UPDATE reviews SET review=:review WHERE id=:id" );
+			$stmt->bindParam ( 'id', $id );
+			$stmd->bindParam ('review', $review);
+			$stmt->execute ();
+		}
+
+		/*
+			MOVIES TABLE FUNCTIONS
+			1) getMovies():
+			2) createMovie():
+			3) updateMovie():
+			4) deleteMovie():
+
+		*/
+
+		
 	} // end class DatabaseAdaptor
 	
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
